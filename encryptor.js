49b4bb56d1f908680db8e809f14cbbd0866e85b0
jeissonj <pescres@gmail.com>
2024-02-18 02:15:33 -0500

    se añadio elemento javascript

---
+++ encryptor.js
@@ -0,0 +1,7 @@
+let tituloCaja2 = document.querySelector("h1");
+tituloCaja2.innerHTML  = "cambio"
+
+function presionEncriptar(){
+    alert('click')
+    let mensajeOriginal = document.getElementById('comentario')
+}
